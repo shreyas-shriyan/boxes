e7573870bc2d46af065c215b16d69e1257d82473
Shreyas Shriyan <51715896+shreyas-shriyan@users.noreply.github.com>
2020-12-17 19:20:00 +0530

    added stacking logic on clicking a box #1

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 
-const start = () => {
-
+const handleClick = (e) => {
+    let item = e.target
+    if (item.innerHTML == "5") {
+        // clicking on box five is unhandled case.
+    }
+    else if (item.style.zIndex == "2") {
+        item.style.zIndex = "0"
+    }
+    else {
+        item.style.zIndex = "2"
+    }
 }
 
-
+const start = () => {
+    let grid = document.getElementById("grid")
+    grid.addEventListener("click", handleClick)
+}
 
 window.addEventListener("load", start)
